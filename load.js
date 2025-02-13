b5f68923d035226a0d22829f671bbfbd4b3ef88c
siddarth2810 <siddarthg0910@gmail.com>
2025-02-13 23:39:08 +0530

    add azure http routes

--- load.js
+++ load.js
@@ -1,34 +1,31 @@
 import http from "k6/http";
 import { check } from "k6";
 
-
 export const options = {
   vus: 100,
-  duration: '30s',
+  duration: "30s",
 };
 
-const BASE_URL = 'https://app.kubesimplify.com'
+const BASE_URL = "https://sid-devops-aks.eastus.cloudapp.azure.com";
 
 function demo() {
   const url = `${BASE_URL}`;
 
-
   let resp = http.get(url);
 
   check(resp, {
-    'endpoint was successful': (resp) => {
+    "endpoint was successful": (resp) => {
       if (resp.status === 200) {
-        console.log(`PASS! url`)
-        return true
+        console.log(`PASS! url`);
+        return true;
       } else {
-        console.error(`FAIL! status-code: ${resp.status}`)
-        return false
+        console.error(`FAIL! status-code: ${resp.status}`);
+        return false;
       }
-    }
+    },
   });
 }
 
-
 export default function () {
-    demo()
+  demo();
 }
